0863ce5caaafcc903cec6295852db82445e4692a
Grzegorz Maciak <grzegorz.maciak@k2.pl>
2017-05-15 13:47:17 +0200

    OpenURL extension doc update

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Extensions
 Usefull extensions which I'm using in my work.
 
-UIApplication+OpenURL - Supports browser selection (optional) and handles case when Safari
-is restricted (disabled) in system settings. Suported browsers Safari, Chrome, Firefox.
+1. [UIApplication+OpenURL](#UIApplication+OpenURL) Usefull in case when safari may be restricted (disabled). Allows to select browser in which http(s) link should be opened.
+
+
+## <a name="UIApplication+OpenURL"> </a>UIApplication+OpenURL
+Supports browser selection (optional) and handles case when Safari	is restricted (disabled) in system settings. Suported browsers:
+
+* Safari,
+* Chrome,
+* Firefox.
+	
+### Browsers other than Safari
+To use other browsers you need to add their schemes to the ***LSApplicationQueriesSchemes*** array
+in the *info.plist* file of your app.
+	
+Add following schemes to support other browsers or case when safari is restricted (disabled):
+	
+	googlechrome  // for http
+	googlechromes // for https
+	firefox
+	
+folowing two are for Opera Mini but they do not work anymore
+	
+	ohttp
+	ohttps
--- UIApplication+OpenURL.h
+++ UIApplication+OpenURL.h
@@ -2,9 +2,33 @@
 //  UIApplication+OpenURL.h
 //
 //
-//  Created by Grzegorz Em on 20.02.2017.
-//  Copyright © 2017 Grzegorz Em. All rights reserved.
+//  Created by Grzegorz Maciak on 20.02.2017.
+//  Copyright © 2017 Grzegorz Maciak. All rights reserved.
 //
+
+/*
+ This code is distributed under the terms and conditions of the MIT license:
+ 
+ Copyright (c) 2017 Grzegorz Maciak
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
 #import <UIKit/UIKit.h>
 
@@ -32,7 +56,7 @@ typedef NS_ENUM(NSUInteger, WebBrowser) {
  googlechromes
  firefox
  
- // folowing two are fro Opera Mini but they do not work anymore
+ // folowing two are for Opera Mini but they do not work anymore
  ohttp
  ohttps
  */
